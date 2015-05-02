93e7ae408c88e6774db0883daf690b2fad7f418d
yongxiang <mao@yongxiang.me>
2015-05-02 11:22:03 +0800

    a

--- getIPaddress.php
+++ getIPaddress.php
@@ -21,3 +21,12 @@ function getIPaddress(){
     }
     return $IPaddress;
 }
+
+ function taobaoIP($clientIP){
+    $taobaoIP = 'http://ip.taobao.com/service/getIpInfo.php?ip='.$clientIP;
+    $IPinfo = json_decode(file_get_contents($taobaoIP));
+    $province = $IPinfo->data->region;
+    $city = $IPinfo->data->city;
+    $data = $province.$city;
+    return $data;
+ }
---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ *
+ * @author mao.yongxiang
+ * @since  2015-05-02
+ */
+$userAgent = @$_SERVER['REMOTE_ADDR'];
+echo '<pre>';
+var_dump($userAgent);
